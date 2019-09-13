b86e43656e0f415c11a05f9f693714d73e83dc65
Bhimsen rao <brkaran2k2@gmail.com>
2019-09-13 13:55:52 +0530

    Add files via upload

---
+++ MongoDB_PPts/startMongo.bat
@@ -0,0 +1,4 @@
+@start D:\myMongo\mongodb-win32-x86_64-2008plus-3.2.1\bin\mongod --dbpath D:\myMongo\mongodb-win32-x86_64-2008plus-3.2.1\data\db
+pause
+@start D:\myMongo\mongodb-win32-x86_64-2008plus-3.2.1\bin\mongo
+pause
